6ed93944a2a573b9161852676f12a5001c170470
Mikhail Loginov <miloginov@mts.ru>
2024-04-30 15:16:20 +0300

    HW3 упростить решение за счет сортировки слайсов

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -9,36 +9,30 @@ func Top10(input string) []string {
 	topCount := 10
 
 	words := strings.Fields(input)
+	if len(words) == 0 {
+		return []string{}
+	}
+
 	countByWord := make(map[string]int, len(words))
 	for _, w := range words {
 		countByWord[w]++
 	}
 
-	wordCounts := make([]int, 0, len(countByWord))
-	wordsByCount := make(map[int][]string, len(countByWord))
-	for k, v := range countByWord {
-		wordsByCount[v] = append(wordsByCount[v], k)
-		if len(wordsByCount[v]) == 1 {
-			wordCounts = append(wordCounts, v)
-		}
+	wordsSlice := make([]string, 0, len(countByWord))
+	for k := range countByWord {
+		wordsSlice = append(wordsSlice, k)
 	}
 
-	sort.Ints(wordCounts)
-	result := make([]string, 0, topCount)
-	counter := 1
-	for i := len(wordCounts) - 1; i >= 0; i-- {
-		if counter > topCount {
-			break
+	sort.Slice(wordsSlice, func(i int, j int) bool {
+		a, b := wordsSlice[i], wordsSlice[j]
+		if countByWord[a] == countByWord[b] {
+			return a < b
 		}
-		sort.Strings(wordsByCount[wordCounts[i]])
-		for _, w := range wordsByCount[wordCounts[i]] {
-			result = append(result, w)
-			counter++
-			if counter > topCount {
-				break
-			}
-		}
-	}
+		return countByWord[a] > countByWord[b]
+	})
 
-	return result
+	if len(wordsSlice) < topCount {
+		topCount = len(wordsSlice)
+	}
+	return wordsSlice[:topCount]
 }
