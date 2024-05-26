29ba326969afff3a8e48811ca4a72db9b6be6e69
Mikhail Loginov <miloginov@mts.ru>
2024-05-27 00:36:14 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,43 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if done != nil {
+		return applyStagesWithCancellation(in, done, stages)
+	}
+
+	return applyStages(in, stages)
+}
+
+func applyStages(in In, stages []Stage) Out {
+	if len(stages) == 0 {
+		return in
+	}
+
+	return applyStages(stages[0](in), stages[1:])
+}
+
+func applyStagesWithCancellation(in In, done In, stages []Stage) Out {
+	in2 := make(Bi)
+
+	go func() {
+		defer close(in2)
+
+		for {
+			select {
+			case m, ok := <-in:
+				if !ok {
+					return
+				}
+				in2 <- m
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	if len(stages) == 0 {
+		return in2
+	}
+
+	return applyStagesWithCancellation(stages[0](in2), done, stages[1:])
 }
